8a68e30953a621519adc0b2348a81fbbfdea5865
Anderson Souza <andersonssilveira96@gmail.com>
2024-12-01 15:01:04 -0300

    Backing authorize

--- src/Api/Controllers/PagamentoController.cs
+++ src/Api/Controllers/PagamentoController.cs
@@ -7,7 +7,7 @@ namespace Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    // [Authorize]
+    [Authorize]
 
     public class PagamentoController : ControllerBase
     {
